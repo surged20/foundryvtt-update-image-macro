47be8e6b7f1b03dde59100bb5acca2373fc9add3
daceblano <daceblano@gmail.com>
2021-09-04 10:29:10 -0700

    Some Fixes
    - fix for url typo
    - fix for custom urls being same when both specified
    - possible fix for replaceAll issue
    - more safety for custom urls
    - added hardcoded url options

--- update-5etools-images.js
+++ update-5etools-images.js
@@ -30,13 +30,13 @@ async function doActors(replaceUrl, newUrl, logging) {
   for (const a of game.actors) {
     let data = {}
     if (a.data?.img?.startsWith(replaceUrl)) {
-      const actorImg = a.data.img.replaceAll(replaceUrl, newUrl);
+      const actorImg = a.data.img.replace(replaceUrl, newUrl);
       data.img = actorImg;
       if (logging) console.log("Actor image: " + actorImg);
     }
 
     if (a.data.token?.img?.startsWith(replaceUrl)) {
-      const tokenImg = a.data.token.img.replaceAll(replaceUrl, newUrl);
+      const tokenImg = a.data.token.img.replace(replaceUrl, newUrl);
       data["token.img"] = tokenImg;
       if (logging) console.log("Actor prototype token: " + tokenImg);
     }
@@ -49,7 +49,7 @@ async function doActors(replaceUrl, newUrl, logging) {
 async function doItems(replaceUrl, newUrl, logging) {
   for (const i of game.items) {
     if (i.data?.img?.startsWith(replaceUrl)) {
-      const img = i.data.img.replaceAll(replaceUrl, newUrl);
+      const img = i.data.img.replace(replaceUrl, newUrl);
       await i.update({"img": img});
       if (logging) console.log("Item image: " + img);
     }
@@ -61,7 +61,7 @@ async function doScenes(updateScenes, replaceUrl, newUrl, logging) {
     const scene = game.scenes.get(sceneKey);
     for (const td of scene.tokens) {
       if (td.data?.img?.startsWith(replaceUrl)) {
-        const img = td.data.img.replaceAll(replaceUrl, newUrl);
+        const img = td.data.img.replace(replaceUrl, newUrl);
         if (logging) console.log("Scene token image: " + img);
         await td.update({"img": img});
       }
@@ -70,6 +70,10 @@ async function doScenes(updateScenes, replaceUrl, newUrl, logging) {
 }
 
 async function doUpdate(updateActors, updateItems, updateScenes, replaceUrl, newUrl, logging) {
+  if(replaceUrl.endsWith('/') == false) { console.log("appending replaceUrl with '/' for safety"); replaceUrl += '/'; }
+  if(newUrl.endsWith('/') == false) { console.log("appending newUrl with '/' for safety"); newUrl += '/'; }
+  if(replaceUrl == newUrl) { console.log('urls specified are the same; updating skipped'); return; }
+
   console.log('Update 5etools Images: started');
   if (updateActors) await doActors(replaceUrl, newUrl, logging);
   if (updateItems) await doItems(replaceUrl, newUrl, logging);
@@ -92,14 +96,17 @@ let content = `<script>
   <hr>
   <p>Default converts 5e.tools image URLs to use the current temporary mirror.</p>
   <label for="replaceUrl">URL to be replaced</label>
-  <select name="replaceUrl" id="replaceUrl" onchange="if(this.options[this.selectedIndex].value=='customUrl'){toggleField(this,this.nextSibling); this.selectedIndex='1';}">
-    <option>https://5e.tools</option>
+  <select name="replaceUrl" id="replaceUrl" onchange="if(this.options[this.selectedIndex].value=='customUrl'){toggleField(this,this.nextSibling); this.selectedIndex=this.options.length-1;}">
+    <option>https://5e.tools/</option>
+    <option>https://5etools-mirror-1.github.io/</option>
+    <option>modules/plutonium/</option>
     <option value="customUrl">[type a custom URL]</option>
-  </select><input name="newUrl" style="display:none;" disabled="disabled" onblur="if(this.value==''){toggleField(this,this.previousSibling);}"><br/>
+  </select><input name="replaceUrl" style="display:none;" disabled="disabled" onblur="if(this.value==''){toggleField(this,this.previousSibling);}"><br/>
   <label for="newUrl">New URL or local path</label>
-  <select name="newUrl" id="newUrl" onchange="if(this.options[this.selectedIndex].value=='customUrl'){toggleField(this,this.nextSibling); this.selectedIndex='2';}">
-    <option>https://5e.tools-mirror-1.github.io</option>
-    <option>modules/plutonium</option>
+  <select name="newUrl" id="newUrl" onchange="if(this.options[this.selectedIndex].value=='customUrl'){toggleField(this,this.nextSibling); this.selectedIndex=this.options.length-1;}">
+    <option>https://5etools-mirror-1.github.io/</option>
+    <option>https://5e.tools/</option>
+    <option>modules/plutonium/</option>
     <option value="customUrl">[type a custom URL]</option>
   </select><input name="newUrl" style="display:none;" disabled="disabled" onblur="if(this.value==''){toggleField(this,this.previousSibling);}">
   <hr>
@@ -130,7 +137,7 @@ new Dialog({
         let updateScenes = html.find("select[name='updateScenes']").val();
         let replaceUrl = html.find("select[name='replaceUrl']").val();
         if (replaceUrl === "customUrl")
-          replaceUrl = html.find("input[name='newUrl']").val();
+          replaceUrl = html.find("input[name='replaceUrl']").val();
         let newUrl = html.find("select[name='newUrl']").val();
         if (newUrl === "customUrl")
           newUrl = html.find("input[name='newUrl']").val();
